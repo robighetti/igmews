6f13b8cfc1b9ff27f2d849960ebc6626d7ac54a7
Rodrigo Bighetti <robighetti@gmail.com>
2022-04-04 07:55:02 -0300

    primeiro upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>world.</span>
+          </h1>
+          <p>
+            Getaccess to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
